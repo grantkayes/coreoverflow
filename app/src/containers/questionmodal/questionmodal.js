fe2d2702f45ba943268d2181b93478e05aeafb1b
Grant Kayes <glk25@georgetown.edu>
2018-07-25 14:25:51 -0700

    creating modal post route

--- app/src/containers/questionmodal/questionmodal.js
+++ app/src/containers/questionmodal/questionmodal.js
@@ -1,23 +1,43 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 // import { push } from 'connected-react-router'
 // import { bindActionCreators } from 'redux'
 // import { connect } from 'react-redux'
 import { Modal, Button, Notation } from '@procore/core-react'
 import { TextEditor, TextArea } from '@procore/core-react'
-import { toggleModal } from '../../modules/sidebar';
+import axios from 'axios'
 
 class QuestionModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  submitQuestion = event => {
+    event.preventDefault();
+
+    const Question = {
+      title: 'Title',
+      body: this.bodyInput
+    };
+
+    axios.post('http://localhost:5000/questions', { Question })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  };
   
   render() {
     console.log(this.props);
       return (
         <Modal open={this.props.open} onClickOverlay={this.props.close}>
           <Modal.Header onClose={this.props.close}>
-            Question Title
-            <TextArea/>
+            Title
+            <TextArea resize="horizontal" ref={(titleInput) => {this.titleInput = titleInput}}/>
           </Modal.Header>
           <Modal.Body>
-            <TextEditor/>
+            <TextEditor onChange={(content) => console.log(content)}/>
           </Modal.Body>
           <Modal.Footer>
             <Modal.FooterNotation>
@@ -27,7 +47,7 @@ class QuestionModal extends React.Component {
               <Button variant="tertiary" onClick={this.props.close}>
                 Cancel
               </Button>
-              <Button variant="primary" onClick={this.props.toggleModal}>
+              <Button variant="primary" onClick={this.submitQuestion}>
                 Submit
               </Button>
             </Modal.FooterButtons>
