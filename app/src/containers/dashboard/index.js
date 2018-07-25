19c275f0ab4bd858cd733bf5891b31b075b40b89
Grant Kayes <glk25@georgetown.edu>
2018-07-25 16:43:47 -0700

    update

--- app/src/containers/dashboard/index.js
+++ app/src/containers/dashboard/index.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import './dashboard.css';
-import QuestionCard from '../../components/questionCard';
-import moment from 'moment';
-import { EmptyState } from '@procore/core-react';
-=======
 import React from 'react'
 import './dashboard.css'
 import QuestionCard from '../../components/questionCard/index'
 import moment from 'moment'
->>>>>>> 53abc6baacd1dcf57308c8dca0f0e35f2be514e5
 import axios from 'axios'
 
 class Dashboard extends React.Component {
@@ -50,17 +42,6 @@ class Dashboard extends React.Component {
       return <QuestionCard key={question.id} question={exampleQuestion} />
     })
 
-<<<<<<< HEAD
-  return (
-    <div id="dashboard">
-      <h1 id="dashboard-header">{props.title}</h1>
-      <div className="questions-wrapper">{allQuestions}</div>
-    </div>
-  );
-};
-
-export default Dashboard;
-=======
     return (
       <div id='dashboard'>
         <h1 id='dashboard-header'>{this.state.title}</h1>
@@ -73,4 +54,3 @@ export default Dashboard;
 }
 
 export default Dashboard
->>>>>>> 53abc6baacd1dcf57308c8dca0f0e35f2be514e5
