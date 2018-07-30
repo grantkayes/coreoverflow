fa6f77a05c95a0aea9990ccad6d81593fafcbb9d
gmcmillion <gregg.mcmillion@procore.com>
2018-07-30 11:09:14 -0700

    Added empty state functionality for a few components

--- app/src/containers/search/index.js
+++ app/src/containers/search/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Dashboard from '../dashboard'
 import { bindActionCreators } from '../../../node_modules/redux'
+import { EmptyState } from '@procore/core-react'
 import { connect } from 'react-redux'
 
 class SearchResults extends React.Component {
   render() {
-    return (
+    return (this.props.searchData.length === 0) ? (
+      <div>
+        <EmptyState>
+          <EmptyState.Image><img src="http://coreoverflow.s3.amazonaws.com/1/5c2398eb-488d-47ec-8b5b-8202a79aca19" alt='empty state img'/></EmptyState.Image>
+          <EmptyState.Title>Sorry, No Search Results</EmptyState.Title>
+          <EmptyState.Description>Please try to search again</EmptyState.Description>
+        </EmptyState>        
+      </div>
+    ) : (
       <div>
         <Dashboard title={"Search Results:"} questionData={this.props.searchData}/>
       </div>
