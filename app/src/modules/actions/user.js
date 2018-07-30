9f2a5ae3cb6229b33e5bba8dc436b64efbd92b47
Chanun Sumphanphaisal <chanun.sumphanphaisal@procore.com>
2018-07-30 11:02:18 -0700

    add user reducers and connected login with redux

---
+++ app/src/modules/actions/user.js
@@ -0,0 +1,23 @@
+import {
+  LOGIN_USER,
+  LOGOUT_USER,
+} from '../reducers/user';
+
+const login = (user) => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: LOGIN_USER,
+      payload: user,
+    });
+  }
+}
+
+const logout = () => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: LOGOUT_USER,
+    });
+  }
+}
+
+export{ login, logout };
--- app/src/modules/index.js
+++ app/src/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import counter from './counter';
 import sidebar from './sidebar';
 import answers from './reducers/answers';
+import user from './reducers/user';
 
 export default combineReducers({
   counter,
   sidebar,
-  answers
+  answers,
+  user
 });
