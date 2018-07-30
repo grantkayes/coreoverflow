c3f25f5268a4eebf670e6c749a2db876ffac9c71
gmcmillion <gregg.mcmillion@procore.com>
2018-07-30 13:55:56 -0700

    Initial code for pagination

--- app/src/containers/dashboard/index.js
+++ app/src/containers/dashboard/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './dashboard.css'
 import QuestionCard from '../../components/questionCard/index'
 import moment from 'moment'
+import { Pagination } from '@procore/core-react'
 
 const Dashboard = props => {
 
@@ -24,6 +25,15 @@ const Dashboard = props => {
       <h1 id='dashboard-header'>{props.title}</h1>
       <div className='questions-wrapper'>
         { allQuestions }
+        <div className='pagination'>
+          <Pagination>
+            <Pagination.Prev />
+            <Pagination.Page >1</Pagination.Page>
+            <Pagination.Page>2</Pagination.Page>
+            <Pagination.Page variant="selected">3</Pagination.Page>
+            <Pagination.Next variant="disabled" />
+          </Pagination>
+        </div>
       </div>
     </div>
   );
