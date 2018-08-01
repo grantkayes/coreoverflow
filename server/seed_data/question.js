438dd4ce3042603f334515c380ddb2b729285bca
Chanun Sumphanphaisal <chanun.sumphanphaisal@procore.com>
2018-08-01 15:06:03 -0700

    rewrote seeders

--- server/seed_data/question.js
+++ server/seed_data/question.js
@@ -1,6 +1,7 @@
 var uuidv4 = require('uuid/v4');
 var randomSentence = require('random-sentence');
 var randomParagraph = require('random-paragraph');
+var moment = require('moment');
 
 var users = [
   {
@@ -26,53 +27,66 @@ function pickRandomUser(users) {
 }
 
 function pickRandomNumber() {
-  return Math.floor(Math.random() * 9 + 1);
+  return Math.floor(Math.random() * 30 + 1);
+}
+
+function pickRandomDate() {
+  var today = moment()
+
+  return moment(today - Math.floor(Math.random()*10000000000))
+}
+
+function pickRandomDateAfter(date) {
+
+  return moment(date + Math.floor(Math.random()*100000000))
+}
+
+function createAnswers(questionId, questionDate) {
+  var ans = {};
+  var ansCount = pickRandomNumber();
+
+  for (var x = 0; x < ansCount; x++) {
+    var ansId = uuidv4();
+
+    var user = pickRandomUser(users);
+
+    var firstName = user.name.substr(0,user.name.indexOf(' '));
+    var lastName = user.name.substr(user.name.indexOf(' ')+1);
+    
+    ans[ansId] = {
+      firstName,
+      lastName,
+      questionId,
+      userEmail: user.email,
+      body: randomParagraph({min: 2, max: 20}),
+      claps: pickRandomNumber(),
+      timestamp: pickRandomDateAfter(questionDate).format('YYYY-MM-DDTHH:mm')
+    }
+  }
+  return ans
 }
 
 function createQuestions(num) {
-  var questionData = [
-    {
-      id: '1',
-      answerCount: 4,
-      questionTitle: 'How do I make this work?',
-      claps: 5,
-      body:
-        'I am not sure what to do to make this work. The issue arise when I do this.',
-      user: 'Elton Xue',
-      userEmail: 'elton.xue@procore.com',
-      timestamp: '2018-07-27T14:21',
-      answers: {}
-    }
-  ];
+  var questionData = [];
 
   for (var x = 0; x < num - 1; x++) {
     var user = pickRandomUser(users);
-    console.log(user)
+    var questionId = uuidv4();
+    var questionDate = pickRandomDate();
+    var ans = createAnswers(questionId, questionDate);
+    var ansCount = Object.keys(ans).length;
 
     questionData.push({
-<<<<<<< HEAD
-      "id": uuidv4(),
-      "answerCount": 4,
+      "id": questionId,
+      "answerCount": ansCount,
       "questionTitle": randomSentence({min: 10, max: 25}),
-      "up": 5,
-      "down": 2,
+      "claps": pickRandomNumber(),
       "body": randomParagraph({min: 1, max: 2}),
       "user": user.name,
       "userEmail": user.email,
-      "timestamp": `2018-07-1${pickRandomNumber()}T14:21`
+      "timestamp": questionDate.format('YYYY-MM-DDTHH:mm'),
+      "answers": ans
     })
-=======
-      id: uuidv4(),
-      answerCount: 4,
-      questionTitle: randomSentence({ min: 10, max: 25 }),
-      claps: 5,
-      body: randomParagraph({ min: 1, max: 20 }),
-      user: user.name,
-      userEmail: user.email,
-      timestamp: '2018-07-27T14:21',
-      answers: {}
-    });
->>>>>>> 48cc258551193f0243e0ef072b02bcecd9b57f84
   }
   return questionData;
 }
