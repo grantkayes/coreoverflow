701ea53fcb09bf886c952611c7e1122b03970a40
Chanun Sumphanphaisal <chanun.sumphanphaisal@procore.com>
2018-08-02 11:34:59 -0700

    add empty state to question with no answers

--- client/src/containers/question/answerList.js
+++ client/src/containers/question/answerList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Header } from '@procore/core-react';
+import { Flex, Header, EmptyState } from '@procore/core-react';
 import Answer from './answer';
 import './index.css';
 import { EDIT_ANSWER_FAILED } from '../../modules/reducers/answers';
@@ -40,7 +40,17 @@ class AnswerList extends React.Component {
         <Header className="answer-list-title" type="h1">
           {this.props.answers.length} Answers
         </Header>
-        {Answers}
+        {
+          this.props.answers.length === 0 ?
+          <div className='empty-answers-container'>
+            <EmptyState>
+              <EmptyState.Image><img src="http://coreoverflow.s3.amazonaws.com/1/5c2398eb-488d-47ec-8b5b-8202a79aca19" alt='empty state img'/></EmptyState.Image>
+              <EmptyState.Title>{`${this.props.userFirstName}, can you answer this question?`}</EmptyState.Title>
+              <EmptyState.Description>Please try using 'Post a Question' in the side bar</EmptyState.Description>
+            </EmptyState>
+          </div> :
+          Answers
+        }
       </Flex>
     );
   }
